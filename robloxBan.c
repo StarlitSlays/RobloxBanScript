01b15e611adb82e3f39b63261341b4eb5a4a4fab
StarlitSlays <114761013+StarlitSlays@users.noreply.github.com>
2023-06-25 12:51:05 +0100

    Create robloxBan.c

---
+++ robloxBan.c
@@ -0,0 +1,7 @@
+curl \
+--location \
+--request POST \
+'https://apis.roblox.com/messaging-service/v1/universes/4805380871/topics/BanPlayer' \
+-H 'x-api-key: ZG8JjxRngE+9ZIBNyc1OrQSp97QTEWTp2rvjBjknAkomcoWC' \
+-H 'Content-Type: application/json' \
+--data-raw '{"message":"message to publish"}'
